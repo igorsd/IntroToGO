c4b86648cf7ab6aac3db67d97473978807e5a7f1
ishtilkind <ishtilkind@github.com>
2021-08-22 14:32:10 -0700

    added videos section that works with json files

--- introduction/part-2.json/videos/main.go
+++ introduction/part-2.json/videos/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world.")
+	videos := getVideos()
+	for i, _ := range videos {
+		videos[i].Description = videos[i].Description + "\n Remember to Like & Subscribe!"
+	}
+	fmt.Println(videos)
+	saveVideos(videos)
 }
